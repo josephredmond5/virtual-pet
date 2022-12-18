3e5bde119f553c5245b0fb3de9f5d2dd9289e3ba
josephredmond5 <josephredmond5@icloud.com>
2022-12-18 19:38:29 +0000

    getting older virtual-pet

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -6,3 +6,21 @@ describe('constructor', () => {
     });
   });
   
+
+  describe('constructor', () => {
+    it('sets the name property', () => {
+      const pet = new Pet('Fido');
+  
+      expect(pet.name).toEqual('Fido');
+    });
+  });
+
+  describe('constructor', () => {
+    it('has a initial age of 1', () => {
+      const pet = new Pet('Fido');
+
+      pet.growUp();
+    
+      expect(pet.age).toEqual(1);
+    });
+  });
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,10 @@
-function Pet() {}
+function Pet(name) {
+    this.name = name;
+    this.age = 0;
+    this.growUp = function() {
+      this.age += 1;
+    };
+  }
 
 module.exports = Pet;
 
-module.exports = Pet;
