a21aef358ca494bdf9fd70a07a8b9b28e308d09c
josephredmond5 <josephredmond5@icloud.com>
2022-12-19 21:13:40 +0000

    Getting Older, Getting Unhealthier - ;TLDR

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,26 +1,47 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
-    });
-  });
+   it('returns an object', () => {
+     expect(new Pet('Fido')).toBeInstanceOf(Object);
+   });
+ });
+
+
+ describe('constructor', () => {
+   it('sets the name property', () => {
+     const pet = new Pet('Fido');
+
+     expect(pet.name).toEqual('Fido');
+   });
+ });
+
+ describe('constructor', () => {
+   it('has a initial age of 1', () => {
+     const pet = new Pet('Fido');
+
+     pet.growUp();
+
+     expect(pet.age).toEqual(1);
+   });
+ });
+
+ describe('growUp', () => {
+  it('increases the age by', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.age).toEqual(1);
+  })
   
+  it('increases the hunger by 5', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.hunger).toEqual(5)
+  })
 
-  describe('constructor', () => {
-    it('sets the name property', () => {
-      const pet = new Pet('Fido');
-  
-      expect(pet.name).toEqual('Fido');
-    });
-  });
+  it('decreases the fitness by 3', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
+  })
 
-  describe('constructor', () => {
-    it('has a initial age of 1', () => {
-      const pet = new Pet('Fido');
-
-      pet.growUp();
-    
-      expect(pet.age).toEqual(1);
-    });
-  });
+ })
